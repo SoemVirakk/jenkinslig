1ee7410cf0b24d61c5691f9542a8e9da50849e96
RADOM KHOEM <138679281+roldophe@users.noreply.github.com>
2024-06-15 22:29:06 +0700

    autopush

--- vars/buildAndPushDocker.groovy
+++ vars/buildAndPushDocker.groovy
@@ -4,14 +4,16 @@ def call(Map config = [:]) {
     def tag = config.get('tag', 'latest')
     def credentialsId = config.get('credentialsId', 'dockerhub-credentials')
 
-    // Write Dockerfile to the workspace
-    def dockerfileContent = libraryResource 'angular.dockerfile'
-    writeFile file: 'angular.dockerfile', text: dockerfileContent
+    // Write Dockerfile content to the workspace
+    def dockerfileContent = libraryResource 'resources/angular.dockerfile'
+    writeFile file: 'Dockerfile', text: dockerfileContent
 
-    withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+    // Access the username and password using the provided credentialsId
+    withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        // Perform Docker login, build, and push commands
         sh """
-            docker login -u ${USERNAME} -p ${PASSWORD}
-            docker build -t ${registry}/${image}:${tag} -f angular.dockerfile .
+            docker login -u $USER -p $PASS
+            docker build -t ${registry}/${image}:${tag} -f Dockerfile .
             docker push ${registry}/${image}:${tag}
         """
     }
